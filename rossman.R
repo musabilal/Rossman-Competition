feba64d7d0bdabeea9a2a0afbb50a0414a75eb1a
musabilal <mbilal@AELAPGM53YY1.LMUK.local>
2015-10-27 17:21:05 +0000

    Some more feature engineering
    Adding another feature using Store,DayOfWeek,StoreType,SchoolHoliday and
    Customers on the given day in store

--- rossman.R
+++ rossman.R
@@ -40,6 +40,7 @@ test$Month<-months(test$Date)
 # removing the date column (since elements are extracted) and also StateHoliday which has a lot of NAs (may add it back in later)
 test <- test[,-c(4,7)]
 
+#Feature using Store,DayOfWeek and Sales
 mean_sales<-summarize(group_by(train,Store,DayOfWeek,add=TRUE),mean(Sales))
 train<-merge(train,mean_sales,by.x=c("Store","DayOfWeek"),by.y=c("Store","DayOfWeek"))
 train$Sales_diff_mean<-round(train$Sales-train$`mean(Sales)`)
@@ -55,4 +56,24 @@ sales_diff_count<-summarize(group_by(train,Store,DayOfWeek,Sales_diff_mean),sum(
 names(sales_diff_count)<-c("Store","DayOfWeek","Sales_diff_mean","total_count_diff")
 train<-merge(train,sales_diff_count)
 train$avg_diff_prob<-train$total_count_diff/train$total_count
-train<-train[,-c(20,21,22)]
+train<-train[,-c(20,21,22)] #Remove the temporary variables created
+train<-train[,-c(3)] #Remove Sales_diff_mean
+
+#Applying this new attribute to the test set
+avg_diff_prob<-summarize(group_by(train,Store,DayOfWeek),max(avg_diff_prob))
+test<-merge(test,avg_diff_prob)
+names(test)[18]<-c("avg_diff_prob")
+
+#Using Store,DayOfWeek,StoreType and checking Customers against it
+
+#On train
+mean_customers<-summarize(group_by(train,Store,DayOfWeek,StoreType,SchoolHoliday),mean(Customers))
+names(mean_customers)<-c("Store","DayOfWeek","StoreType","SchoolHoliday","feature_2")
+train<-merge(train,mean_customers)
+train$Customer_difference<-abs(train$Customers-train$feature_2)/train$Customers
+train<-train[,-which(names(train) %in% c("feature_2"))]
+train$Customer_difference[is.infinite(train$Customer_difference)]<-0
+
+#On test
+mean_customers_test<-train[,which(names(train) %in% c("Store","DayOfWeek","StoreType","SchoolHoliday","Customer_difference"))]
+test<-merge(test,mean_customers)
